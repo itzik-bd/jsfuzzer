69093bb9c5981e204164086cac2099c075b7d462
Shahar Avital <shahar7878@gmail.com>
2015-05-09 19:49:32 +0300

    Running sampleProg on SpiderMonkey Engine

--- jsfuzzer/src/Main.java
+++ jsfuzzer/src/Main.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import Engines.SpiderMonkeyEngine;
+
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		System.out.println("Hello jsfuzzer\n");
 		
@@ -8,5 +12,15 @@ public class Main
 		
 		String progStr = JST.Vistors.JstToJs.execute(p);
 		System.out.println(progStr);
+		
+		PrintWriter writer = new PrintWriter("file1.js", "UTF-8");
+		writer.println(progStr);
+		writer.close();
+		
+		String fileToRun = "file2.js";
+		
+		SpiderMonkeyEngine spiderMonkey = new SpiderMonkeyEngine();
+		spiderMonkey.runFile(fileToRun);
 	}
+	
 }
